26b08f350f11befc1e64793dbf1bfc981289a689
Daniel Rubio <sevendus@gmail.com>
2021-03-09 23:19:26 +0100

    First commit

---
+++ src/ios/SASGMACustomEventBanner.h
@@ -0,0 +1,17 @@
+//
+//  SASGMACustomEventBanner.h
+//
+//  Created by Julien Gomez on 21/06/16.
+//  Copyright © 2018 Smart AdServer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SASGMACustomEventBanner : NSObject <GADCustomEventBanner>
+
+@end
+
+NS_ASSUME_NONNULL_END
